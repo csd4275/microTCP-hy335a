0c3eb058742d12303d987cdf01e2df6663ca50ed
orchiot <orchiot@ics.forth.gr>
2021-11-30 14:54:36 +0200

    first commit

---
+++ test/test_microtcp_server.c
@@ -0,0 +1,66 @@
+/*
+ * microtcp, a lightweight implementation of TCP for teaching,
+ * and academic purposes.
+ *
+ * Copyright (C) 2015-2017  Manolis Surligas <surligas@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * You can use this file to write a test microTCP server.
+ * This file is already inserted at the build system.
+ */
+
+
+#include "../lib/microtcp.h"
+
+#include <netinet/ip.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STR_ERROR "\033[1;31mERROR\033[93m:\033[0m"
+
+void check_main_input(int argc, char ** argv);
+
+int main(int argc, char ** argv)
+{
+    microtcp_sock_t ssock;
+    struct sockaddr_in addr;
+    uint16_t port;
+
+
+    check_main_input(argc, argv);
+    printf("initializing server...\n");
+    ssock = microtcp_socket(AF_INET, SOCK_DGRAM, 0);
+
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port        = htons(port);
+    addr.sin_family      = AF_INET;
+
+    microtcp_bind(&ssock, (struct sockaddr *)(&addr), sizeof(addr));
+    close(ssock.sd);
+
+    return 0;
+}
+
+void check_main_input(int argc, char ** argv) {
+
+    if ( argc < 2 ) {
+
+        printf(STR_ERROR " unspecified port number\n");
+        printf("./test_microtcp_server <port-number>\n");
+        exit(EXIT_FAILURE);
+    }
+}
---
+++ utils/errorc.h
@@ -0,0 +1,26 @@
+#ifndef _UTILS_ERRORC_H
+#define _UTILS_ERRORC_H
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#define check(x) _check(x, __LINE__, __FUNCTION__);
+#define ENABLE_DEBUG_MSG
+
+static inline void _check(int retval, int line, char * funct){
+
+    if ( retval < 0 ) {
+
+        /** TODO: terminate connections ? */
+
+        #ifdef ENABLE_DEBUG_MSG
+        printf("\033[93m%d\033[0m::\033[0;91m%s\033[0m() failed: \033[4m%s\033[0m\n", line, funct, strerror(errno));
+        #endif
+        exit(EXIT_FAILURE);
+    }
+}
+
+#endif /* _UTILS_ERRORC_H */
